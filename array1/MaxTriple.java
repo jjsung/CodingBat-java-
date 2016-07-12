c90c1590b1eff577ba073ef098bc8edbd04cfc46
JJSung <krnforsale@gmail.com>
2016-07-12 17:38:46 -0400

    MaxTriple, Middleway

---
+++ array1/MaxTriple.java
@@ -0,0 +1,24 @@
+package array1;
+/*
+ *Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+
+maxTriple([1, 2, 3])  3
+maxTriple([1, 5, 3])  5
+maxTriple([5, 2, 3])  5
+ */
+public class MaxTriple {
+	public int maxTriple(int[] nums) {
+		  
+		int first= nums[0];
+		int middle= nums[nums.length/2 ];
+		int last=nums[nums.length-1];
+		
+		if (first>middle && first>last)	
+			return first;
+		if (middle>first && middle>last )
+			return middle;
+		else
+			return last;
+	}
+
+}
---
+++ array1/Middleway.java
@@ -0,0 +1,18 @@
+package array1;
+/*
+ given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
+
+middleWay([1, 2, 3], [4, 5, 6])  [2, 5]
+middleWay([7, 7, 7], [3, 8, 0])  [7, 8]
+middleWay([5, 2, 9], [1, 4, 5]) [2, 4]
+ */
+public class Middleway {
+
+	public int[] middleWay(int[] a, int[] b) {
+		int[] abc = { a[1], b[1] };
+	    return abc;
+	 
+	}
+
+	
+}
