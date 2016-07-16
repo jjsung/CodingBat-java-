b646c977a046ba687e11825e9e0812b9ae9edff3
JJSung <krnforsale@gmail.com>
2016-07-16 16:05:23 -0400

    Either 23, Evenodd,Fizarray3

---
+++ array2/Either24.java
@@ -0,0 +1,30 @@
+package array2;
+/*
+ * Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
+
+either24([1, 2, 2]) → true
+either24([4, 4, 1]) → true
+either24([4, 4, 1, 2, 2]) → false
+ */
+public class Either24 {
+	public boolean either24(int[] nums) {
+		  
+		boolean two=false;
+		boolean four=false;
+		
+		for(int i=0; i<nums.length-1;i++)
+		{
+			if (nums[i]==2 && nums[i+1]==2)
+				two=true;
+			
+			if (nums[i]==4 && nums[i+1]==4)
+				four=true;
+		}
+		if(two==true && four==true)
+			return false;
+		return two||four; 
+		
+		
+	}
+
+}
