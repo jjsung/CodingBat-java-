4f8694b98985ceb64f3ce2777f4466828c1a149c
JJSung <krnforsale@gmail.com>
2016-07-24 13:13:36 -0400

    Tenrun,Trippleup,testing

---
+++ array2/testing.java
@@ -0,0 +1,14 @@
+package array2;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean truth1= true;
+		boolean truth2= false;
+		
+		System.out.println(truth1 || truth2);
+	}
+
+}
