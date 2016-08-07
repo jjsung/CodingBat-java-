5ceca5aad1f630b5b41c6fa486133328899529bd
JJSung <krnforsale@gmail.com>
2016-08-07 17:28:16 -0400

    Factorial,Fibnoacci

---
+++ recurision1/Factorial.java
@@ -0,0 +1,13 @@
+package recursion1;
+
+public class Factorial {
+	public int factorial(int n) {
+		
+		if(n==1)
+			return n;
+		
+		return n * factorial(n-1);
+		
+	}
+
+}
---
+++ recurision1/Fibonacci.java
@@ -0,0 +1,17 @@
+package recursion1;
+
+public class Fibonacci {
+
+	public int fibonacci(int n) {
+		 
+		if (n <= 1) 
+			return n;
+	    
+		return fibonacci(n - 2) + fibonacci(n - 1);
+		
+		
+	}
+
+	
+	
+}
