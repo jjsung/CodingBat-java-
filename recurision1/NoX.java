6fee7ab424cbf4cd1a97deb229b96227b76dda7c
JJSung <krnforsale@gmail.com>
2016-08-08 12:08:41 -0400

    NestParen,Nox

---
+++ recurision1/NoX.java
@@ -0,0 +1,12 @@
+package recursion1;
+
+public class NoX {
+	public String noX(String str) {
+		  
+		if(str.length()<1) return str;
+		
+		if(str.charAt(0)=='x') return noX(str.substring(1));
+		return str.charAt(0)+ noX(str.substring(1));
+	}
+
+}
