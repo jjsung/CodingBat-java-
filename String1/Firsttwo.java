56c4f80e43e265a766edf3ba9f3f79377634088c
JJSung <krnforsale@gmail.com>
2016-08-21 11:03:14 -0400

    Firsttwo

---
+++ String1/Firsttwo.java
@@ -0,0 +1,13 @@
+package String1;
+
+public class Firsttwo {
+	public String firstTwo(String str) {
+
+		if (str.length()<=2)
+				return str;
+		else
+			return str.substring(0, 2);
+		
+	}
+
+}
---
+++ String1/firsthalf.java
@@ -0,0 +1,11 @@
+package String1;
+
+public class firsthalf {
+	public String firstHalf(String str) {
+		
+		
+		
+		return str.substring(0,str.length()/2 );
+	}
+
+}
