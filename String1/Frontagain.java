efc4a5e1cf05105f61aa42d577595cc87161088c
JJSung <krnforsale@gmail.com>
2016-08-22 10:55:32 -0400

    Frontagain,Hasbad

---
+++ String1/Frontagain.java
@@ -0,0 +1,14 @@
+package String1;
+
+public class Frontagain {
+	public boolean frontAgain(String str) {
+		  
+		if (str.length()<2)
+			return false;
+		
+		return str.substring(0, 2).equals(str.substring(str.length()-2));
+		  
+		  
+	}
+
+}
---
+++ String1/Hasbad.java
@@ -0,0 +1,19 @@
+package String1;
+
+public class Hasbad {
+	public boolean hasBad(String str) {
+		
+		if(str.length()<3)
+			return false; 
+		
+		if(str.length()==3)
+			return  (str.substring(0).equals("bad") ); 
+		
+		
+		return (str.substring(0,3).equals("bad") ||  str.substring(1,4).equals("bad")      );
+			
+		
+		
+	}
+
+}
