72bf6f31f87c2739a0ebf27f00687257ae4eb43c
JJSung <krnforsale@gmail.com>
2016-08-23 10:11:19 -0400

    Helloname,Lastchars

---
+++ String1/Helloname.java
@@ -0,0 +1,19 @@
+package String1;
+/*
+ * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+helloName("Bob") → "Hello Bob!"
+helloName("Alice") → "Hello Alice!"
+helloName("X") → "Hello X!"
+
+ */
+public class Helloname {
+	public String helloName(String name) {
+		  
+		return "Hello " + name + "!"; 
+		
+		
+		
+	}
+
+}
