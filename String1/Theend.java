1fc6dff7d6ab05d978e0483362d5830c5f339dbb
JJSung <krnforsale@gmail.com>
2016-08-30 09:58:51 -0400

    Startword,Theend

---
+++ String1/Theend.java
@@ -0,0 +1,13 @@
+package String1;
+
+public class Theend {
+	public String theEnd(String str, boolean front) {
+	
+		if(front==true)
+			return str.substring(0,1);
+		else
+			return str.substring(str.length()-1);		
+		
+	}
+
+}
