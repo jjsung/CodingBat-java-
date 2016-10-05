90f2936c079a59a76ea857bde851577e30237e2d
JJSung <krnforsale@gmail.com>
2016-10-05 12:06:15 -0400

    mapBully

---
+++ Maps1/mapBully.java
@@ -0,0 +1,12 @@
+package map1;
+
+public class mapBully {
+	
+	public Map<String, String> mapBully(Map<String, String> map) {
+		  if(map.containsKey("a")) {
+		    map.put("b", map.get("a"));
+		    map.put("a", "");
+		  }
+		  return map;
+		}
+}
