22b782eeb403d02e1318ba4a290625dc5b7cf240
JJSung <krnforsale@gmail.com>
2016-10-07 19:35:01 -0400

    mapAB

---
+++ Maps1/mapAb.java
@@ -0,0 +1,10 @@
+package map1;
+
+public class mapAb {
+	public Map<String, String> mapAB(Map<String, String> map) {
+		   if( map.containsKey("a") && map.containsKey("b")){
+		     map.put("ab",(map.get("a") + map.get("b")));
+		  }
+		 return map;
+		}
+}
