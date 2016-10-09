5c7425a360516ee9e03697405c11c3b3b02b403e
JJSung <krnforsale@gmail.com>
2016-10-09 15:30:38 -0400

    topping1

---
+++ Maps1/topping1.java
@@ -0,0 +1,11 @@
+package map1;
+
+public class topping1 {
+	public Map<String, String> topping1(Map<String, String> map) {
+		  if(map.containsKey("ice cream")){
+		    map.put("ice cream", "cherry");
+		  }
+		  map.put("bread", "butter");
+		  return map;
+		}
+}
