d436f1767466682d8ca7734f8a10c209babd2c3e
JJSung <krnforsale@gmail.com>
2016-10-21 12:56:15 -0400

    word0

---
+++ Maps2/word0.java
@@ -0,0 +1,13 @@
+package CodingBat2;
+
+public class word0 {
+	public Map<String, Integer> word0(String[] strings) {
+		   
+		  Map<String, Integer> map = new HashMap<String, Integer>();
+		   
+		  for (int i = 0; i < strings.length; i++) {
+		    map.put(strings[i], 0);
+		  }
+		  return map;
+		}
+}
