642dbfbf6e946b8b1b834012bd5298aa476278f5
JJSung <krnforsale@gmail.com>
2016-11-09 20:58:16 -0500

    Pairs

---
+++ Maps2/Pairs.java
@@ -0,0 +1,17 @@
+package map2;
+
+public class Pairs {
+	public Map<String, String> pairs(String[] strings) {
+
+		Map<String, String> map = new HashMap();
+		for (String s: strings){
+		map.put(s.substring(0,1), s.substring(s.length()-1, s.length()));
+		}
+		return map;
+
+		}
+
+
+		   
+
+}
